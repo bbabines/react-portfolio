909167b520bff303b7eef2d77283f199ca66383a
unknown <bbabines@gmail.com>
2022-12-09 14:53:51 -0500

    Navbar almost finished. Need onClick to be moved to custom component

---
+++ src/components/darkmode-button/darkmode-button.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const DarkModeButton = () => {
+	return (
+		<div className="toggleWrapper">
+			<input type="checkbox" className="dn" id="dn" />
+			<label htmlFor="dn" className="toggle">
+				<span className="toggle__handler">
+					<span className="crater crater--1"></span>
+					<span className="crater crater--2"></span>
+					<span className="crater crater--3"></span>
+				</span>
+				<span className="star star--1"></span>
+				<span className="star star--2"></span>
+				<span className="star star--3"></span>
+				<span className="star star--4"></span>
+				<span className="star star--5"></span>
+				<span className="star star--6"></span>
+			</label>
+		</div>
+	);
+};
+
+export default DarkModeButton;
--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import DarkModeButton from "../darkmode-button/darkmode-button.component";
 
 const NavBar = (props) => {
 	return (
 		<header className={props.darkMode ? "navbar" : "nav--light"}>
-			<button type="button" onClick={props.toggleDarkMode} className="mode">
-				Btn
-			</button>
 			<h3 className={props.darkMode ? "signature" : "signature--light"}>
 				<a href="google.com"> Brad Babines</a>
 			</h3>
+
+			<div onClick={props.toggleDarkMode} className="dark-mode-button">
+				<DarkModeButton />
+			</div>
+
+			{/* <button type="button" onClick={props.toggleDarkMode} className="mode">
+				Dark Mode
+			</button> */}
 
 			<p className={props.darkMode ? "nav-links" : "nav-links--light"}>
 				<a href="google.com">Home</a>
