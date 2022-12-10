fbf1bb16a6b9c840d2567fd805c3c3a0f406d2d8
Tammy Wilson <bbabines@gmail.com>
2022-12-10 00:54:16 -0500

    Finished Darkmode

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import DarkModeButton from "../darkmode-button/darkmode-button.component";
 
 const NavBar = (props) => {
 	return (
@@ -8,14 +7,35 @@ const NavBar = (props) => {
 				<a href="google.com"> Brad Babines</a>
 			</h3>
 
-			<div onClick={props.toggleDarkMode} className="dark-mode-button">
-				<DarkModeButton />
+			{/* Dark mode button */}
+			<div className="toggleWrapper" id="dark-mode-button">
+				<input
+					type="checkbox"
+					className="dn"
+					id="dn"
+					onClick={props.toggleDarkMode}
+				/>
+				<label htmlFor="dn" className="toggle">
+					<span className="toggle__handler">
+						<span className="crater crater--1"></span>
+						<span className="crater crater--2"></span>
+						<span className="crater crater--3"></span>
+					</span>
+					<span className="star star--1"></span>
+					<span className="star star--2"></span>
+					<span className="star star--3"></span>
+					<span className="star star--4"></span>
+					<span className="star star--5"></span>
+					<span className="star star--6"></span>
+				</label>
 			</div>
 
+			{/* <div onClick={props.toggleDarkMode} className="dark-mode-button">
+				<DarkModeButton />
+			</div> */}
 			{/* <button type="button" onClick={props.toggleDarkMode} className="mode">
 				Dark Mode
 			</button> */}
-
 			<p className={props.darkMode ? "nav-links" : "nav-links--light"}>
 				<a href="google.com">Home</a>
 			</p>
